9d3f6c7ec6b478de2f08fea9fdb3f7744de90909
unknown <trongdatga@gmail.com>
2024-12-12 09:25:00 +0700

    commit to pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -344,7 +344,7 @@ export default function EnhancedTable() {
 
   /* === function cus === */
   const fetchData = async () => {
-    const res = await axios.get("http://192.168.30.107:3000/post/api");
+    const res = await axios.get("http://localhost:3000/post/api");
     setListData(res.data.data);
   };
   React.useEffect(() => {
